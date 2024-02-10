fd217ea8fe5e54d02dce56ee22259619e4ab8b88
venkatesh276849 <159613455+venkatesh276849@users.noreply.github.com>
2024-02-10 14:55:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class helloworld
 {
 	public static void main(string args[])
 	{
-		System.out.println("Hello World! Welcome to Devops Training"):
+		System.out.println("Hello World! Welcome to Devops Training");
 	}
 }
 
