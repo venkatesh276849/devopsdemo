d3e6e7a89e138757c6e00c3e6984178aa3b11d6a
venkatesh276849 <159613455+venkatesh276849@users.noreply.github.com>
2024-02-10 15:02:03 +0530

    Update HelloWorld.java
    Subtraction of two variables

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,10 @@ public class helloworld
 	public static void main(string args[])
 	{
 		System.out.println("Hello World! Welcome to Devops Training");
+		int a = 20;
+		int b = 10;
+		int c = a-b;
+		System.out.println ("Subtract a-b="c);
 	}
 }
 
